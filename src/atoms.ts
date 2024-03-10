85c8d310f115aa8d8b25ba0dd3355fec3643844c
OKilberg <oscar.oravakilberg@gmail.com>
2024-03-10 18:37:10 +0100

    attempted fixes for gameSession not updating, will continue next time

--- src/atoms.ts
+++ src/atoms.ts
@@ -5,6 +5,8 @@ import { HexGridShapes } from "../game-state-model/models/HexGrid";
 import {MovementController, newMovementController} from '../game-state-model/controllers/MovementController'
 import { newGameSessionController } from "../game-state-model/controllers/GameSessionController";
 import { SingletonLog } from "../game-state-model/models/Log";
+import { atomWithListeners } from "./customAtoms/atomWithListeners";
+import { focusAtom } from "jotai-optics";
 
 type View = 'setup' | 'play'
 export type TileSize = 'small' | 'medium'
@@ -19,7 +21,9 @@ export const modeAtom = atom('standard')
 export const heroesAtom = atom<Hero[]>([])
 export const enemiesAtom = atom<Enemy[]>([])
 export const gameStateAtom = atom<GameState>(gameState)
-export const gameStateControllerAtom = atom(gSController)
+export const gameSessionControllerAtom = atom(gSController)
+export const gameSessionAtom = focusAtom(gameStateAtom, (optic)=>optic.prop('gameStateData').prop('gameSession'))
+//export const sessionListenerAtom = atomWithListeners(gSController)
 export const logAtom = atom(log)
 //export const tileActorMapAtom = atom<TileActorMap>((get)=>get(gameStateAtom).gameStateData.tileActorMap)
 export const movementControllerAtom = atom<MovementController>((get)=>newMovementController(get(gameStateAtom),get(logAtom)))
--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -1,7 +1,9 @@
 import HexTile, { HexRow } from "../hex/HexTile"
 import { useAtom } from "jotai"
-import { gameStateAtom, selectedActorAtom, tileSizeAtom, viewAtom } from "../atoms"
+import { gameSessionAtom, gameSessionControllerAtom, gameStateAtom, selectedActorAtom, tileSizeAtom, viewAtom } from "../atoms"
 import { IoMdExit } from "react-icons/io";
+import { IoPlaySkipForwardCircle } from "react-icons/io5";
+
 import { Actor, Hero } from "../../game-state-model/models/Actor";
 import { useState } from "react";
 
@@ -11,19 +13,21 @@ export default function Play({ }: Props) {
     const [view, setView] = useAtom(viewAtom)
     const [gameState] = useAtom(gameStateAtom)
     const [tileSize] = useAtom(tileSizeAtom)
+    const [gSC] = useAtom(gameSessionControllerAtom)
+    const [gameSession] = useAtom(gameSessionAtom)
     const [selectedActor, selectActor] = useAtom(selectedActorAtom)
 
-    function renderHexTiles(){
-        if(!gameState) return;
+    function renderHexTiles() {
+        if (!gameState) return;
         const tiles = gameState.gameStateData.hexGrid.toArray();
         const width = 8
         const height = 8
         const rows = []
-        for (let i = 0; i < height; i++){
+        for (let i = 0; i < height; i++) {
             const row = []
-            while (row.length < width && tiles.length > 0){
+            while (row.length < width && tiles.length > 0) {
                 const tile = tiles.shift()
-                if(tile) row.push(<HexTile tile={tile} size={tileSize}/>)
+                if (tile) row.push(<HexTile tile={tile} size={tileSize} />)
             }
             rows.push(<HexRow offset={i % 2 ? true : false} size={tileSize}>{row}</HexRow>)
         }
@@ -32,13 +36,25 @@ export default function Play({ }: Props) {
 
     return (
         <main className="h-screen w-screen flex flex-col items-center  gap-4 p-4">
-            <header className="bg-zinc-700 flex items-center w-full min-h-[40px] px-2">
-                <button onClick={()=>setView('setup')} className="flex items-center gap-1 text-lg hover:opacity-75 cursor-pointer"><IoMdExit size={24} className="rotate-180 text-red-400"/> Exit</button>
+            <header className="bg-zinc-700 flex items-center justify-between w-full min-h-[40px] px-2">
+                <button onClick={() => setView('setup')} className="flex items-center gap-1 text-lg hover:opacity-75 cursor-pointer">
+                    <IoMdExit size={24} className="rotate-180 text-red-400" />
+                    Exit
+                </button>
+                <div className="flex gap-4 items-center">
+                    <h2 className="text-lg">Round: {gSC.getRound()}</h2>
+                    <h2 className="text-lg"><b>Turn: {gSC.getTurn()}</b></h2>
+                    <button className="bg-orange-500 p-1 rounded-md flex items-center px-2 gap-1" onClick={()=>gSC.endRound()}>End Round <IoPlaySkipForwardCircle size={24}/> </button>
+                </div>
+                <div>
+
+                </div>
+
             </header>
             <section className="grid grid-cols-12 w-full h-full max-h-full">
                 <div className="outline outline-green-600 w-full h-full flex flex-col items-center col-span-1 px-2">
                     <h2 className="flex border-b-2">Players</h2>
-                    {gameState.getHeroes().map(actor=><UnitContainer actor={actor}/>)}
+                    {gameState.getHeroes().map(actor => <UnitContainer actor={actor} />)}
                 </div>
                 <div className="col-span-10 flex w-full items-start justify-center">
                     <div className="scale-100">
@@ -46,11 +62,11 @@ export default function Play({ }: Props) {
                             renderHexTiles()
                         }
                     </div>
-                    
+
                 </div>
                 <div className="outline outline-red-600 w-full h-full flex flex-col items-center col-span-1 px-2">
                     <h2 className="flex border-b-2">Enemies</h2>
-                    {gameState.getEnemies().map(actor=><UnitContainer actor={actor}/>)}
+                    {gameState.getEnemies().map(actor => <UnitContainer actor={actor} />)}
                 </div>
             </section>
             <section className="w-full">
@@ -60,25 +76,25 @@ export default function Play({ }: Props) {
     )
 }
 
-function UnitContainer({actor}: {actor: Actor}){
-    const {id, name, health, maxHealth, moves, maxMoves} = actor
-    const {hero} = actor as Hero
+function UnitContainer({ actor }: { actor: Actor }) {
+    const { id, name, health, maxHealth, moves, maxMoves } = actor
+    const { hero } = actor as Hero
     const [selectedActor, selectActor] = useAtom(selectedActorAtom)
 
     const selectedStyle = "bg-blue-500"
     const defaultStyle = `outline outline-grey-300 w-full min-h-[125px] ${selectedActor?.id === id && selectedStyle}`
     return (
-        <div onClick={()=>selectActor(actor)} className={defaultStyle}>
+        <div onClick={() => selectActor(actor)} className={defaultStyle}>
             {
                 hero ? <p>{hero} ({name})</p> : <p>{name} (Enemy{id})</p>
             }
             <p>{health}/{maxHealth}HP</p>
-            <p>Moves Left: {maxMoves-moves}</p>
+            <p>Moves Left: {maxMoves - moves}</p>
         </div>
     )
 }
 
-function mapToArray(map: Map<any,any>) {
+function mapToArray(map: Map<any, any>) {
     return Array.from(map.entries()).map(([key, value]) => {
         return value;
     });
